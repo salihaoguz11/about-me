570b0e47d6bba11ec64820ba65f4c72094938221
Saliha <salihaoguz2011@gmail.com>
2023-03-04 23:33:27 +0000

    navstyled

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,13 @@
 import "./Nav.styled";
-import { Div } from "./Nav.styled";
-import { Link } from "react-router-dom";
+import { Div, Links } from "./Nav.styled";
 
 const Nav = () => {
   return (
     <Div>
-      <Link to="/">Home</Link>
-      <Link to="/myproject">My Projects</Link>
-      <Link to="/myskills">My Skills</Link>
-      <Link to="/contact">Contact</Link>
+      <Links to="/">Home</Links>
+      <Links to="/myproject">My Projects</Links>
+      <Links to="/myskills">My Skills</Links>
+      <Links to="/contact">Contact</Links>
     </Div>
   );
 };
--- src/components/nav/Nav.styled.jsx
+++ src/components/nav/Nav.styled.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-
+import { Link } from "react-router-dom";
 export const Div = styled.div`
   background-color: #b4cf66;
   width: 100%;
@@ -7,4 +7,17 @@ export const Div = styled.div`
   display: flex;
   justify-content: space-evenly;
   align-items: center;
+`;
+
+export const Links = styled(Link)`
+  text-decoration: none;
+  cursor: pointer;
+  transform: scale(1.1);
+  transition: all 3s;
+  &:hover {
+    color: #ff5a33;
+
+    transition: all 1s ease;
+    transform: scale(1.2);
+  }
 `;
